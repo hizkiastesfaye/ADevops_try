aa5c4c34a43c787d0b344e82e34066e5fe9f27cd
hizkiastesfaye <tesfayehizkias@gmail.com>
2024-09-05 10:59:01 +0300

    edit test

--- test_firstproj.py
+++ test_firstproj.py
@@ -1,12 +1,13 @@
 import pytest
 from firstproj import speak
 
+
 def test_speak(capsys):
     # Call the function
     speak()
-    
+
     # Capture the output
     captured = capsys.readouterr()
-    
+
     # Assert that the output is correct
     assert captured.out == "11\n"
